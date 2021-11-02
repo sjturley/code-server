a5f4055474f0e9c5b747926b24f0d2e73d9dbddd
Steve Turley <steve@turleylabs.com>
2021-11-02 08:03:59 -0500

    Update

--- start-code-server.sh
+++ start-code-server.sh
@@ -4,6 +4,7 @@ project_volume=$3
 config_volume=$4
 
 docker run -it -d --rm -p 0.0.0.0:$port:8080 \
+ -v "/var/run/docker.sock:/var/run/docker.sock" \
  -v "$config_volume:/home/coder/.config" \
  -v "$project_volume:/home/coder/project" \
  -u "$(id -u):$(id -g)" \
